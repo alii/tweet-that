864a2858fe3e2eb1fa00c1fed4671a432de2b296
Rmaughan93 <rhys.maughan93@gmail.com>
2021-04-06 22:36:25 +0100

    merged

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,13 +1,23 @@
 import {Prisma, PrismaClient, User} from "@prisma/client";
+import {wrapRedis} from "./redis";
 
 export const prisma = new PrismaClient();
 
 export async function findOneByDiscordId(
   discord_id: string
 ): Promise<Prisma.Prisma__UserClient<User | null>> {
-  return await prisma.user.findFirst({
-    where: {discord_id},
+  return wrapRedis(`user:${discord_id}`, () => {
+    return prisma.user.findFirst({
+      where: {discord_id},
+    });
   });
 }
 
-export async function findOneByUidAndDiscordId(discord_id: string, uid: string) {}
+export async function findOneByUidAndDiscordId(
+  discord_id: string,
+  uid: string
+): Promise<Prisma.Prisma__UserClient<User | null>> {
+  return wrapRedis(`user:${discord_id}:${uid}`, () => {
+    return prisma.user.findFirst({where: {AND: [{discord_id}, {uid}]}});
+  });
+}
