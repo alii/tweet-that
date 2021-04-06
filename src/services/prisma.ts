694b7a00a733f635b5e1a8a0d55ea45555f3792f
Rmaughan93 <rhys.maughan93@gmail.com>
2021-04-06 22:31:15 +0100

    merging:

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,9 +1,13 @@
-import {PrismaClient} from "@prisma/client";
+import {Prisma, PrismaClient, User} from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
-export async function findOneByDiscordId(discord_id: string) {
+export async function findOneByDiscordId(
+  discord_id: string
+): Promise<Prisma.Prisma__UserClient<User | null>> {
   return await prisma.user.findFirst({
     where: {discord_id},
   });
 }
+
+export async function findOneByUidAndDiscordId(discord_id: string, uid: string) {}
